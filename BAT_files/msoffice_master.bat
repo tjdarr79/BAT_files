eaf3f8d27a9b64ee0aaf179d02c553f9018234e5
tjdarr79 <tjdarr79@gmail.com>
2017-03-13 16:10:22 -0700

    add msoffice bat file
    this bat file will get the MSoffice last 5 product key .

---
+++ BAT_files/msoffice_master.bat
@@ -0,0 +1,4 @@
+@echo off
+psexec @C:\Users\travisd\Desktop\test\pc.txt cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus >> "C:\Users\travisd\Desktop\test\test.txt"
+pause
+
